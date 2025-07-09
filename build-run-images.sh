0b8ca5f1a5e1f51ff89ee6e9f85b08ffd1879379
adityaval317 <adityavaliveti317@gmail.com>
2025-07-09 05:33:16 -0400

    Microservices Kafka and Rabbit MQ

--- build-run-images.sh
+++ build-run-images.sh
@@ -17,29 +17,41 @@ PROFILE=${4:-"default"}
 
 current_dir=$(pwd)
 echo "Current directory: $current_dir"
-if docker ps | grep -w "$REPOSITORY_ACCOUNT*"; then
-  echo "Waiting for Docker containers to stop..."
-  cd "$current_dir/$PROFILE"
-  docker-compose down
-  # sleep 20
-fi
+
 cd "$current_dir"
-microservices=("accounts" "cards" "loans" "config-server" "eureka-server" "gateway-server")
+stop_and_remove_all_containers=1
+microservices=("accounts" "cards" "loans" "config-server" "eureka-server" "gateway-server" "notification-service")
 # If the microservice name is provided, filter the list
 if [ "$MICROSERVICE_NAME" != "all" ]; then
   microservices=("$MICROSERVICE_NAME")
+  stop_and_remove_all_containers=0
 fi
+
+if docker ps | grep -w "$REPOSITORY_ACCOUNT*"; then
+  echo "Waiting for Docker containers to stop..."
+  cd "$current_dir/$PROFILE"
+  if [ $stop_and_remove_all_containers -eq 0 ]; then
+    # Stop and remove one or more specific microservices
+    # CONTAINER_ID=$(docker ps --filter "ancestor=$REPOSITORY_ACCOUNT/${microservices[@]}:$OLD_BUILD_VERSION" --format "{{.ID}}")
+    docker-compose rm "${microservices[@]}"
+    # sleep 20
+  else
+    docker-compose down
+  fi
+  # sleep 20
+fi
+cd "$current_dir"
 # Navigate to each directory and run mvn compile jib:dockerBuild
 for service in "${microservices[@]}"; do
   # Check if docker containers are running and stop them
 
   # Check if a docker image exists with old version
-  if docker images "$REPOSITORY_ACCOUNT/$service:$OLD_BUILD_VERSION"; then
+  if docker images | grep "$REPOSITORY_ACCOUNT/$service:$OLD_BUILD_VERSION"; then
     echo "Removing old Docker image for $REPOSITORY_ACCOUNT/$service:$OLD_BUILD_VERSION"
     docker rmi "$REPOSITORY_ACCOUNT/$service:$OLD_BUILD_VERSION" || true
   fi
   echo "Building Docker image for $service with version $BUILD_VERSION"
-  cd "../$service/"
+  cd "../$service"
   mvn compile jib:dockerBuild -Dbuild.image.version=$BUILD_VERSION
   while ! docker images "$REPOSITORY_ACCOUNT/$service:$BUILD_VERSION" > /dev/null; do
     echo "Waiting for Docker image to be built for $REPOSITORY_ACCOUNT/$service:$BUILD_VERSION..."
